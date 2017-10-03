e59ba04bcf7d990dba54deff5b6606baf0f5bab1
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2017-10-03 12:02:10 -0400

    Add requirements.txt

--- kraken.py
+++ kraken.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 from crontab import CronTab
-import sys, os, yaml
+import sys, os, yaml, time
 import optparse
 import random
 import subprocess
 import ConfigParser
 import tempfile
 import requests
-from openshift import client, config
-#from kubernetes import client, config
+#from openshift import client, config
+from kubernetes import client, config
 
 nodes = []
 master_nodes = []
 namespace = "default"
-kube_cfg =  os.path.join(os.environ["HOME"], '.kube/config'))
+kube_cfg =  os.path.join(os.environ["HOME"], '.kube/config')
 cli = client.CoreV1Api()
-config.load_kube_config("%s") %(kube_cfg)
+#cli = client.OapiApi()
+config.load_kube_config()
 
 def help():
     print "Usage: monkey --config <path-to-config-file>"
@@ -35,7 +36,7 @@ def check_count(before_count, after_count):
 def node_pod_count(node, label):
     pods = []
     if label == deleted:
-        cmd = "oadm manage node %s --list-pods" %(node)
+        cmd = "oadm manage-node %s --list-pods" %(node)
     else:
         cmd = "oc get pods --all-namespaces"
     with open("/tmp/pods") as pods_file:
@@ -44,7 +45,7 @@ def node_pod_count(node, label):
         for pod in get_pods:
             if pod is None:
                 pass
-            else
+            else:
                 pods.append(line.split(' ')[0])
     pod_count = len(pods)
     pods_file.close()
@@ -53,11 +54,11 @@ def node_pod_count(node, label):
 def pod_count(namespace):
     pods = []
     config.load_kube_config("%s") %(kube_cfg)
-    pods_list = cli.list_namespaced_pod("%s") %(namespace, watch=False)
+    pods_list = cli.list_pod_for_all_namespaces(namespace, watch=False)
     for pod in pods_list.items:
         pods.append(pod.status.pod_ip)
     pod_count = len(pods)
-return pod_count
+    return pod_count
 
 def check_master(picked_node):
     ret = cli.list_node(pretty=True, label_selector="type=master")
@@ -68,18 +69,18 @@ def check_master(picked_node):
         check_master(random_node)
     return random_node
 
-def get_random_node():
-    ret = cli.list_node(pretty=True)
+def get_random_node(label):
+    ret = cli.list_node(pretty=True, label_selector=label)
     for data in ret.items:
         nodes.append(data.metadata.name)
     # pick random node to kill
     random_node = random.choice(nodes)
-return random_node
+    return random_node
 
-def monkey(project_name):
+def monkey(label):
     # leave master node out
     # pick random node to kill
-    random_node = get_random_node()
+    random_node = get_random_node(label)
     random_node = check_master(random_node)
     # get pod count on the node before deleting the node
     before_pods = node_pod_count(random_node, label)
@@ -93,11 +94,19 @@ def monkey(project_name):
         print "something went wrong, node didn't get deleted"
     # pod count after deleting the node
     pod_count_after = pod_count(project_name, kube_cfg)
+    sleep_counter = 0
     # check if the pods have been rescheduled
-    status = check_count(before_pods, pod_count_after)
-    if not status:
-        print "Test failed, looks like pods haven't been rescheduled"
-        sys.exit(1)
+    while True:
+        print "checking if the pods have been rescheduled"
+        time.sleep(60)
+        status = check_count(before_pods, pod_count_after)
+        if status:
+            print "Test passed, pods have been been rescheduled"
+            break
+        sleep_counter = int(sleep_counter)+60
+        if int(sleep_counter) > 900:
+            print "Test failed, looks like pods haven't been rescheduled after waiting for 900 seconds"
+            sys.exit(1)
 
 def main(cfg, kube_cfg):
     #parse config
@@ -105,7 +114,11 @@ def main(cfg, kube_cfg):
         config = ConfigParser.ConfigParser()
         config.read(cfg)
         namespace = config.get('projects','name')
-        monkey(namespace, kube_cfg)
+        label = config.get('projects', 'label')
+        if label is None:
+            monkey()
+        else:
+            monkey(label)
         gopath = config.get('set-env','gopath')
     else:
         help()
