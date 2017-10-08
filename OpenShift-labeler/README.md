55f342f6b306af040d025b450f01750aa58983ea
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2017-10-08 19:59:09 -0400

    poll apiserver every second for the status, timeout after 30 seconds

--- OpenShift-labeler/README.md
+++ OpenShift-labeler/README.md
@@ -32,12 +32,12 @@ The playbook looks for kube config in Home directory and copies the config from 
 ### Labeling of nodes
 The nodes are labeled as follows depending on the type of the role they play in OpenShift cluster:
 
-- masters - type=master
-- etcd - type=etcd
-- nodes - type=node
-- glusterfs - type=cns
-- lb - type=lb
-- infra - type=infra
+- masters - node_type=master
+- etcd - node_type=etcd
+- nodes - node_type=node
+- glusterfs - node_type=cns
+- lb - node_type=lb
+- infra - node_type=infra
 
 ### pbench
-when using containerized pbench, we need to label all the nodes with type=pbench. Setting label_pbench to True will label all the nodes.
+when using containerized pbench, we need to label all the nodes with node_type=pbench. Setting label_pbench to True will label all the nodes.
--- kraken.py
+++ kraken.py
@@ -15,10 +15,11 @@ from colorama import Fore, Back, Style
 
 nodes = []
 master_nodes = []
-kube_cfg =  os.path.join(os.environ["HOME"], '.kube/config')
+#kube_cfg =  os.path.join(os.environ["HOME"], '.kube/config')
 config.load_kube_config()
 cli = client.CoreV1Api()
 body = client.V1DeleteOptions()
+poll_timeout = 30
 #cli = client.OapiApi()
 init()
 
@@ -48,7 +49,7 @@ def pod_count():
     count = len(pods)
     return count
 
-def check_master(picked_node):
+def check_master(picked_node, master_label):
     ret = cli.list_node(pretty=True, label_selector=master_label)
     for data in ret.items:
         master_nodes.append(data.metadata.name)
@@ -76,13 +77,13 @@ def node_pod_count(node):
         get_pods = pods_file.readlines()[1:]
     return len(get_pods)
 
-def monkey(label):
+def monkey(label, master_label):
     # get list of nodes
     list_nodes(label)
     # leave master node out
     # pick random node to kill
     random_node = get_random_node(label)
-    random_node = check_master(random_node)
+    random_node = check_master(random_node, master_label)
     # count number of pods before deleting the node
     pod_count_node = node_pod_count(random_node)
     pod_count_before = pod_count()
@@ -91,52 +92,55 @@ def monkey(label):
     print (Fore.GREEN + 'deleting %s\n') %(random_node)
     cli.delete_node(random_node, body)
     #check if the node is taken out
-    delete_counter = 0
+    delete_counter = 1
     while True:
         print (Fore.YELLOW + 'waiting for %s to get deleted\n') %(random_node)
-        time.sleep(60)
+        time.sleep(delete_counter)
         if random_node in list_nodes(label):
-            delete_counter = delete_counter+60
+            delete_counter = delete_counter+1
         else:
-            print (Fore.GREEN + '%s deleted\n') %(random_node)
+            print (Fore.GREEN + '%s deleted. It took approximately %s seconds\n') %(random_node, delete_counter)
             break
-        if delete_counter > 120:
-            print (Fore.RED + 'something went wrong, node did not get deleted\n')
+        if delete_counter > poll_timeout:
+            print (Fore.RED + 'something went wrong, node did not get deleted after waiting for %s seconds\n') %(delete_counter)
             sys.exit(1)
     # pod count after deleting the node
     pod_count_after = pod_count()
-    sleep_counter = 0
+    sleep_counter = 1
     # check if the pods have been rescheduled
     while True:
         print (Fore.YELLOW + 'checking if the pods have been rescheduled\n')
-        time.sleep(60)
+        time.sleep(sleep_counter)
         status = check_count(pod_count_before, pod_count_after)
         if status:
-            print (Fore.GREEN 'Test passed, pods have been been rescheduled\n')
+            print (Fore.GREEN + 'Test passed, pods have been been rescheduled. It took approximately %s seconds\n') %(sleep_counter)
             break
-        sleep_counter = sleep_counter+60
-        if sleep_counter > 900:
-            print (Fore.RED + 'Test failed, looks like pods have not been rescheduled after waiting for 900 seconds\n')
+        sleep_counter = sleep_counter+1
+        if sleep_counter > poll_timeout:
+            print (Fore.RED + 'Test failed, looks like pods have not been rescheduled after waiting for %s seconds\n') %(sleep_counter)
+            print (Fore.YELLOW + 'Test ended at %s UTC') $(datetime.datetime.utcnow())
             sys.exit(1)
+        print (Fore.YELLOW + 'Test ended at %s UTC') $(datetime.datetime.utcnow())
 
 def main(cfg):
     #parse config
     if os.path.isfile(cfg):
         config = ConfigParser.ConfigParser()
         config.read(cfg)
-        namespace = config.get('projects','name')
-        label = config.get('projects', 'label')
-        master_label = config.get('projects', 'master_label')
+        namespace = config.get('kraken','name')
+        label = config.get('kraken', 'label')
+        master_label = config.get('kraken', 'master_label')
         if (label is None):
             print (Fore.YELLOW + 'label is not provided, assuming you are okay with deleting any of the available nodes except the master\n')
             label = "undefined"
-        monkey(label)
+        monkey(label, master_label)
         gopath = config.get('set-env','gopath')
     else:
         help()
         sys.exit(1)
 
 if __name__ == "__main__":
+    print (Fore.YELLOW + 'starting the test at %s UTC') %(datetime.datetime.utcnow())
     parser = optparse.OptionParser()
     parser.add_option("-c", "--config", dest="cfg", help="path to the config")
     (options, args) = parser.parse_args()
